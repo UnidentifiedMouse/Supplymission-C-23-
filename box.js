46d127a55f383aadee5db30a0606e2709eebf94c
UnidentifiedMouse <68704445+UnidentifiedMouse@users.noreply.github.com>
2020-08-20 10:05:58 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box{
+    this.body.Bodies = rectangle(200,400,200,20,options);
+    this.body.Bodies = rectangle(100,350,20,100,options);
+    this.body.Bodies = rectangle(300,350,20,100,options);
+     constructor();{
+         var options = {
+             isStatic:true,
+              restitution: 0.8,
+              friction: 0.3,
+              density: 1.0
+              World.add(world,this.body);
+         }
+         this.body
+         this.height
+         this.width
+
+    }
+
+
+
+
+}
